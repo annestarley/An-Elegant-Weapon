532c22ba398dc213de12427d720267e41fb38680
Anne Starley <annestarley@gmail.com>
2018-04-25 14:10:13 -0700

    cleaned up images folder

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ button0.addEventListener('click', function() {
   imgs[0].setAttribute('id', 'senator')
   imgs[0].setAttribute('id', 'job')
   divs[0].setAttribute('class', 'vocation')
-  imgs[0].setAttribute('src', 'images/senator.jpg')
+  imgs[0].setAttribute('src', 'images/vocation/senator.jpg')
   imgs[0].setAttribute('alt', 'senator')
   h4s[0].innerText = 'Galactic Senator'
   container.appendChild(divs[0])
@@ -93,7 +93,7 @@ button0.addEventListener('click', function() {
   imgs[1].setAttribute('id', 'bountyhunter')
   imgs[1].setAttribute('id', 'job')
   divs[1].setAttribute('class', 'vocation')
-  imgs[1].setAttribute('src', 'images/bountyhunter.jpg')
+  imgs[1].setAttribute('src', 'images/vocation/bountyhunter.jpg')
   imgs[1].setAttribute('alt', 'bounty-hunter')
   h4s[1].innerText = 'Bounty Hunter'
   container.appendChild(divs[1])
@@ -102,7 +102,7 @@ button0.addEventListener('click', function() {
   imgs[2].setAttribute('id', 'musician')
   imgs[2].setAttribute('id', 'job')
   divs[2].setAttribute('class', 'vocation')
-  imgs[2].setAttribute('src', 'images/musician.jpg')
+  imgs[2].setAttribute('src', 'images/vocation/musician.jpg')
   imgs[2].setAttribute('alt', 'musician')
   h4s[2].innerText = 'Musician'
   container.appendChild(divs[2])
@@ -111,7 +111,7 @@ button0.addEventListener('click', function() {
   imgs[3].setAttribute('id', 'beauty')
   imgs[3].setAttribute('id', 'job')
   divs[3].setAttribute('class', 'vocation')
-  imgs[3].setAttribute('src', 'images/beauty.png')
+  imgs[3].setAttribute('src', 'images/vocation/beauty.png')
   imgs[3].setAttribute('alt', 'beauty-contestant')
   h4s[3].innerText = 'Beauty Contestant'
   container.appendChild(divs[3])
@@ -120,7 +120,7 @@ button0.addEventListener('click', function() {
   imgs[4].setAttribute('id', 'pilot')
   imgs[4].setAttribute('id', 'job')
   divs[4].setAttribute('class', 'vocation')
-  imgs[4].setAttribute('src', 'images/pilot.jpeg')
+  imgs[4].setAttribute('src', 'images/vocation/pilot.jpeg')
   imgs[4].setAttribute('alt', 'pilot')
   h4s[4].innerText = 'Pilot'
   container.appendChild(divs[4])
@@ -302,70 +302,70 @@ button2.addEventListener('click', function() {
     h4s2.push(h4)
   }
   imgs2[0].setAttribute('id', 'movies')
-  imgs2[0].setAttribute('src', 'images/episode1.png')
+  imgs2[0].setAttribute('src', 'images/movies/episode1.png')
   imgs2[0].setAttribute('alt', 'episode 1')
   h4s2[0].innerText = 'Episode 1'
   divs2[0].appendChild(imgs2[0])
   divs2[0].appendChild(h4s2[0])
   div3.appendChild(divs2[0])
   imgs2[1].setAttribute('id', 'movies')
-  imgs2[1].setAttribute('src', 'images/episode2.jpg')
+  imgs2[1].setAttribute('src', 'images/movies/episode2.jpg')
   imgs2[1].setAttribute('alt', 'episode 2')
   h4s2[1].innerText = 'Episode 2'
   divs2[1].appendChild(imgs2[1])
   divs2[1].appendChild(h4s2[1])
   div3.appendChild(divs2[1])
   imgs2[2].setAttribute('id', 'movies')
-  imgs2[2].setAttribute('src', 'images/clonewars.jpg')
+  imgs2[2].setAttribute('src', 'images/movies/clonewars.jpg')
   imgs2[2].setAttribute('alt', 'clone wars')
   h4s2[2].innerText = 'Clone Wars'
   divs2[2].appendChild(imgs2[2])
   divs2[2].appendChild(h4s2[2])
   div3.appendChild(divs2[2])
   imgs2[3].setAttribute('id', 'movies')
-  imgs2[3].setAttribute('src', 'images/episode3.jpg')
+  imgs2[3].setAttribute('src', 'images/movies/episode3.jpg')
   imgs2[3].setAttribute('alt', 'episode 3')
   h4s2[3].innerText = 'Episode 3'
   divs2[3].appendChild(imgs2[3])
   divs2[3].appendChild(h4s2[3])
   div3.appendChild(divs2[3])
   imgs2[4].setAttribute('id', 'movies')
-  imgs2[4].setAttribute('src', 'images/rogueone.png')
+  imgs2[4].setAttribute('src', 'images/movies/rogueone.png')
   imgs2[4].setAttribute('alt', 'Rogue One')
   h4s2[4].innerText = 'Rogue One'
   divs2[4].appendChild(imgs2[4])
   divs2[4].appendChild(h4s2[4])
   div3.appendChild(divs2[4])
   imgs2[5].setAttribute('id', 'movies')
-  imgs2[5].setAttribute('src', 'images/episode4.jpg')
+  imgs2[5].setAttribute('src', 'images/movies/episode4.jpg')
   imgs2[5].setAttribute('alt', 'episode 4')
   h4s2[5].innerText = 'Episode 4'
   divs2[5].appendChild(imgs2[5])
   divs2[5].appendChild(h4s2[5])
   div3.appendChild(divs2[5])
   imgs2[6].setAttribute('id', 'movies')
-  imgs2[6].setAttribute('src', 'images/episode5.jpg')
+  imgs2[6].setAttribute('src', 'images/movies/episode5.jpg')
   imgs2[6].setAttribute('alt', 'episode 5')
   h4s2[6].innerText = 'Episode 5'
   divs2[6].appendChild(imgs2[6])
   divs2[6].appendChild(h4s2[6])
   div3.appendChild(divs2[6])
   imgs2[7].setAttribute('id', 'movies')
-  imgs2[7].setAttribute('src', 'images/episode6.jpg')
+  imgs2[7].setAttribute('src', 'images/movies/episode6.jpg')
   imgs2[7].setAttribute('alt', 'episode 6')
   h4s2[7].innerText = 'Episode 6'
   divs2[7].appendChild(imgs2[7])
   divs2[7].appendChild(h4s2[7])
   div3.appendChild(divs2[7])
   imgs2[8].setAttribute('id', 'movies')
-  imgs2[8].setAttribute('src', 'images/episode7.jpg')
+  imgs2[8].setAttribute('src', 'images/movies/episode7.jpg')
   imgs2[8].setAttribute('alt', 'episode 7')
   h4s2[8].innerText = 'Episode 7'
   divs2[8].appendChild(imgs2[8])
   divs2[8].appendChild(h4s2[8])
   div3.appendChild(divs2[8])
   imgs2[9].setAttribute('id', 'movies')
-  imgs2[9].setAttribute('src', 'images/episode8.jpg')
+  imgs2[9].setAttribute('src', 'images/movies/episode8.jpg')
   imgs2[9].setAttribute('alt', 'episode 8')
   h4s2[9].innerText = 'Episode 8'
   divs2[9].appendChild(imgs2[9])
@@ -535,7 +535,7 @@ button4.addEventListener('click', function() {
   imgs5[0].setAttribute('id', 'felucia')
   imgs5[0].setAttribute('id', 'vacation-spot')
   divs5[0].setAttribute('class', 'vacation')
-  imgs5[0].setAttribute('src', 'images/felucia.jpeg')
+  imgs5[0].setAttribute('src', 'images/vacation/felucia.jpeg')
   imgs5[0].setAttribute('alt', 'felucia')
   h4s5[0].innerText = 'Felucia'
   container5.appendChild(divs5[0])
@@ -544,7 +544,7 @@ button4.addEventListener('click', function() {
   imgs5[1].setAttribute('id', 'naboo')
   imgs5[1].setAttribute('id', 'vacation-spot')
   divs5[1].setAttribute('class', 'vacation')
-  imgs5[1].setAttribute('src', 'images/naboo.jpeg')
+  imgs5[1].setAttribute('src', 'images/vacation/naboo.jpeg')
   imgs5[1].setAttribute('alt', 'naboo')
   h4s5[1].innerText = 'Naboo'
   container5.appendChild(divs5[1])
@@ -553,7 +553,7 @@ button4.addEventListener('click', function() {
   imgs5[2].setAttribute('id', 'alderaan')
   imgs5[2].setAttribute('id', 'vacation-spot')
   divs5[2].setAttribute('class', 'vacation')
-  imgs5[2].setAttribute('src', 'images/alderaan.jpg')
+  imgs5[2].setAttribute('src', 'images/vacation/alderaan.jpg')
   imgs5[2].setAttribute('alt', 'alderaan')
   h4s5[2].innerText = 'Alderann (pre-blow-up)'
   container5.appendChild(divs5[2])
@@ -562,7 +562,7 @@ button4.addEventListener('click', function() {
   imgs5[3].setAttribute('id', 'endor')
   imgs5[3].setAttribute('id', 'vacation-spot')
   divs5[3].setAttribute('class', 'vacation')
-  imgs5[3].setAttribute('src', 'images/endor.jpg')
+  imgs5[3].setAttribute('src', 'images/vacation/endor.jpg')
   imgs5[3].setAttribute('alt', 'endor')
   h4s5[3].innerText = 'Endor'
   container5.appendChild(divs5[3])
@@ -571,7 +571,7 @@ button4.addEventListener('click', function() {
   imgs5[4].setAttribute('id', 'coruscant')
   imgs5[4].setAttribute('id', 'vacation-spot')
   divs5[4].setAttribute('class', 'vacation')
-  imgs5[4].setAttribute('src', 'images/coruscant.jpeg')
+  imgs5[4].setAttribute('src', 'images/vacation/coruscant.jpeg')
   imgs5[4].setAttribute('alt', 'coruscant')
   h4s5[4].innerText = 'Coruscant'
   container5.appendChild(divs5[4])
@@ -656,7 +656,7 @@ button5.addEventListener('click', function() {
   }
 
   var r2d2 = document.createElement('img')
-  r2d2.setAttribute('src', 'images/r2d2.jpg')
+  r2d2.setAttribute('src', 'images/droids/r2d2.jpg')
   r2d2.setAttribute('alt', 'R2D2')
   r2d2.setAttribute('id', 'r2d2')
   r2d2.setAttribute('id', 'droid-type')
@@ -673,7 +673,7 @@ button5.addEventListener('click', function() {
   droidcontainer.appendChild(droid[1])
 
   var bb8 = document.createElement('img')
-  bb8.setAttribute('src', 'images/bb8.jpg')
+  bb8.setAttribute('src', 'images/droids/bb8.jpg')
   bb8.setAttribute('alt', 'BB8')
   bb8.setAttribute('id', 'bb8')
   bb8.setAttribute('id', 'droid-type')
@@ -770,24 +770,24 @@ button6.addEventListener('click', function() {
 
   if (answers[0] === '' && answers[1] === '' && answers[2] === '' && answers[3] === '' && answers[4] === '') {
     ansHeader.innerText = 'BLASTER'
-    ansImg.setAttribute('src', 'images/blaster.png')
+    ansImg.setAttribute('src', 'images/weapons/blaster.png')
     ansImg.setAttribute('alt', 'blaster')
     ansParagraph.innerText = "Looks like your not a force wielder... a blaster is the weapon for you! (...such an uncivilized weapon)"
   } else if (count > 2) {
     ansHeader.innerText = 'DARKSABER'
-    ansImg.setAttribute('src', 'images/darksaber.jpg')
+    ansImg.setAttribute('src', 'images/weapons/darksaber.jpg')
     ansImg.setAttribute('alt', 'darksaber')
     ansParagraph.innerText = "An ancient and unique lightsaber design with only one previously created."
   } else if (answers[0] === 'senator') {
     if (answers[3] === ('coruscant' || 'alderaan')) {
       ansHeader.innerText = 'GREEN DOUBLE-BLADED LIGHTSABER'
-      ansImg.setAttribute('src', 'images/greendouble.jpg')
+      ansImg.setAttribute('src', 'images/weapons/greendouble.jpg')
       ansImg.setAttribute('alt', 'green double-bladed lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
       ansParagraph.innerText = "A single hilt with a beam emmiter at both ends, the double-bladed sword was first invented by the Sith empire but the design has since been used by the Jedi."
     } else {
       ansHeader.innerText = 'GREEN SINGLE-BLADED LIGHTSABER'
-      ansImg.setAttribute('src', 'images/greensingle.jpg')
+      ansImg.setAttribute('src', 'images/weapons/greensingle.jpg')
       ansImg.setAttribute('alt', 'green single-bladed lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
       ansParagraph.innerText = ""
@@ -795,13 +795,13 @@ button6.addEventListener('click', function() {
   } else if (answers[0] === 'pilot') {
     if (answers[3] === ('endor' || 'felucia')) {
       ansHeader.innerText = 'BLUE DOUBLE-BLADED LIGHTSABER'
-      ansImg.setAttribute('src', 'images/bluedouble.jpg')
+      ansImg.setAttribute('src', 'images/weapons/bluedouble.jpg')
       ansImg.setAttribute('alt', 'blue double-bladed lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
       ansParagraph.innerText = ""
     } else {
       ansHeader.innerText = 'BLUE SINGLE-BLADED LIGHTSABER'
-      ansImg.setAttribute('src', 'images/bluesingle.jpg')
+      ansImg.setAttribute('src', 'images/weapons/bluesingle.jpg')
       ansImg.setAttribute('alt', 'blue single-bladed lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
       ansParagraph.innerText = ""
@@ -809,109 +809,109 @@ button6.addEventListener('click', function() {
   } else if (answers[0] === 'bounty-hunter') {
     if (answers[3] === ('naboo' || 'felucia')) {
       ansHeader.innerText = 'RED DOUBLE-BLADED LIGHTSABER'
-      ansImg.setAttribute('src', 'images/reddouble.jpg')
+      ansImg.setAttribute('src', 'images/weapons/reddouble.jpg')
       ansImg.setAttribute('alt', 'red double-bladed lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
       ansParagraph.innerText = ""
     } else if (answers[3] === 'endor') {
       ansHeader.innerText = 'RED CROSS-GUARD LIGHTSABER'
-      ansImg.setAttribute('src', 'images/redcrossgaurd.png')
+      ansImg.setAttribute('src', 'images/weapons/redcrossgaurd.png')
       ansImg.setAttribute('alt', 'red cross-guard lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
       ansParagraph.innerText = ""
     } else {
       ansHeader.innerText = 'RED SINGLE-BLADED LIGHTSABER'
-      ansImg.setAttribute('src', 'images/redsingle.png')
+      ansImg.setAttribute('src', 'images/weapons/redsingle.png')
       ansImg.setAttribute('alt', 'red single-bladed lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
       ansParagraph.innerText = ""
     }
   } else if (answers[0] === 'musician' && answers[2] === 'yah') {
     ansHeader.innerText = 'PURPLE SINGLE-BLADED LIGHTSABER'
-    ansImg.setAttribute('src', 'images/purple.jpg')
+    ansImg.setAttribute('src', 'images/weapons/purple.jpg')
     ansImg.setAttribute('alt', 'purple single-bladed lightsaber')
     ansImg.setAttribute('style', 'height: 300px;')
     ansParagraph.innerText = ""
   } else if (answers[0] === 'musician' && answers[2] === 'respect') {
     if (answers[4] === ('BB8')) {
       ansHeader.innerText = 'YELLOW PIKE-STAFF LIGHTSABER'
-      ansImg.setAttribute('src', 'images/yellowdouble.png')
+      ansImg.setAttribute('src', 'images/weapons/yellowdouble.png')
       ansImg.setAttribute('alt', 'yellow pike-staff lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
       ansParagraph.innerText = ""
     } else {
       ansHeader.innerText = 'WHITE SINGLE-BLADED LIGHTSABER'
-      ansImg.setAttribute('src', 'images/whitesingle.jpg')
+      ansImg.setAttribute('src', 'images/weapons/whitesingle.jpg')
       ansImg.setAttribute('alt', 'white single-bladed lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
       ansParagraph.innerText = ""
     }
   } else if (answers[0] === 'musician' && answers[2] === '') {
     ansHeader.innerText = 'YELLOW PIKE-STAFF LIGHTSABER'
-    ansImg.setAttribute('src', 'images/yellowdouble.png')
+    ansImg.setAttribute('src', 'images/weapons/yellowdouble.png')
     ansImg.setAttribute('alt', 'yellow pike-staff lightsaber')
     ansImg.setAttribute('style', 'height: 300px;')
     ansParagraph.innerText = ""
   } else if (answers[0] === 'musician' && answers[2] === 'pass') {
     ansHeader.innerText = 'YELLOW SINGLE-BLADED LIGHTSABER'
-    ansImg.setAttribute('src', 'images/yellowsingle.jpg')
+    ansImg.setAttribute('src', 'images/weapons/yellowsingle.jpg')
     ansImg.setAttribute('alt', 'yellow single-bladed lightsaber')
     ansImg.setAttribute('style', 'height: 300px;')
     ansParagraph.innerText = ""
   } else if (answers[0] === 'beauty-contestant' && answers[2] === 'respect') {
     ansHeader.innerText = 'PURPLE SINGLE-BLADED LIGHTSABER'
-    ansImg.setAttribute('src', 'images/purple.jpg')
+    ansImg.setAttribute('src', 'images/weapons/purple.jpg')
     ansImg.setAttribute('alt', 'purple single-bladed lightsaber')
     ansImg.setAttribute('style', 'height: 300px;')
     ansParagraph.innerText = ""
   } else if (answers[0] === 'beauty-contestant' && answers[2] === 'yah') {
     ansHeader.innerText = 'WHITE SINGLE-BLADED LIGHTSABER'
-    ansImg.setAttribute('src', 'images/whitesingle.jpg')
+    ansImg.setAttribute('src', 'images/weapons/whitesingle.jpg')
     ansImg.setAttribute('alt', 'white single-bladed lightsaber')
     ansImg.setAttribute('style', 'height: 300px;')
     ansParagraph.innerText = ""
   } else if (answers[0] === 'beauty-contestant' && answers[2] === ('pass' || '')) {
     if (answers[3] === ('coruscant' || 'naboo')) {
       ansHeader.innerText = 'YELLOW PIKE-STAFF LIGHTSABER'
-      ansImg.setAttribute('src', 'images/yellowdouble.png')
+      ansImg.setAttribute('src', 'images/weapons/yellowdouble.png')
       ansImg.setAttribute('alt', 'yellow pike-staff lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
       ansParagraph.innerText = "Used by Jedi Temple guards sworn to protect Coruscants temple."
     } else {
       ansHeader.innerText = 'YELLOW SINGLE-BLADED LIGHTSABER'
-      ansImg.setAttribute('src', 'images/yellowsingle.jpg')
+      ansImg.setAttribute('src', 'images/weapons/yellowsingle.jpg')
       ansImg.setAttribute('alt', 'yellow single-bladed lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
     }
   } else if (answers[0] === '' && answers[2] === 'yah') {
     ansHeader.innerText = 'WHITE SINGLE-BLADED LIGHTSABER'
-    ansImg.setAttribute('src', 'images/whitesingle.jpg')
+    ansImg.setAttribute('src', 'images/weapons/whitesingle.jpg')
     ansImg.setAttribute('alt', 'white single-bladed lightsaber')
     ansImg.setAttribute('style', 'height: 300px;')
     ansParagraph.innerText = ""
   } else if (answers[0] === '' && answers[2] === 'respect') {
     if (answers[4] === 'R2D2') {
       ansHeader.innerText = 'YELLOW PIKE-STAFF LIGHTSABER'
-      ansImg.setAttribute('src', 'images/yellowdouble.png')
+      ansImg.setAttribute('src', 'images/weapons/yellowdouble.png')
       ansImg.setAttribute('alt', 'yellow pike-staff lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
       ansParagraph.innerText = ""
     } else {
       ansHeader.innerText = 'YELLOW SINGLE-BLADED LIGHTSABER'
-      ansImg.setAttribute('src', 'images/yellowsingle.jpg')
+      ansImg.setAttribute('src', 'images/weapons/yellowsingle.jpg')
       ansImg.setAttribute('alt', 'yellow single-bladed lightsaber')
       ansImg.setAttribute('style', 'height: 300px;')
     }
   } else if (answers[0] === '' && answers[2] === ('' || 'pass')) {
     ansHeader.innerText = 'PURPLE SINGLE-BLADED LIGHTSABER'
-    ansImg.setAttribute('src', 'images/purple.jpg')
+    ansImg.setAttribute('src', 'images/weapons/purple.jpg')
     ansImg.setAttribute('alt', 'purple single-bladed lightsaber')
     ansImg.setAttribute('style', 'height: 300px;')
     ansParagraph.innerText = ""
   } else {
     alert('default')
     ansHeader.innerText = 'BLUE SINGLE-BLADED LIGHTSABER'
-    ansImg.setAttribute('src', 'images/bluesingle.jpg')
+    ansImg.setAttribute('src', 'images/weapons/bluesingle.jpg')
     ansImg.setAttribute('alt', 'blue single-bladed lightsaber')
     ansImg.setAttribute('style', 'height: 300px;')
     ansParagraph.innerText = ""
